7b4db1d9a3899b7e8345e7a9ddcb4d73d91a2714
Tamara Schymura <tamara.schymura@posteo.net>
2026-04-23 19:03:59 +0200

    Create initial resources.tf

--- resources.tf
+++ resources.tf
@@ -4,13 +4,12 @@ resource "railway_project" "classifieds-lifecycle" {
   name = "classifieds-lifecycle"
 }
 
-# resource "railway_environment" "classifieds-lifecycle" {
-#   name = "classifieds-lifecycle"
-#   project_id = railway_project.classifieds-lifecycle.id
-#
-# }
+resource "railway_service" "scraper" {
+  name       = "classifieds-scraper"
+  project_id = railway_project.classifieds-lifecycle.id
+}
 
-resource "railway_service" "example" {
-  name       = "api"
+resource "railway_service" "db" {
+  name       = "Postgres"
   project_id = railway_project.classifieds-lifecycle.id
 }
