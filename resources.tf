83bafa6e37d15ae05fd1fdd850079e17cca0b5f4
Tamara Schymura <tamara.schymura@posteo.net>
2026-04-28 10:14:01 +0200

    move generated imports into resource.tf

--- resources.tf
+++ resources.tf
@@ -13,18 +13,28 @@
 #   to = railway_service.db
 #   id = "91673c0b-c1a8-45ce-9eb2-5666d2aa63c0"
 # }
+
+
+import {
+  to = railway_variable.scraper_datasource_url
+  id = "2bbcb9ae-18da-4429-a43f-ef80f4af1178"
+}
+
+resource "railway_variable" "scraper_datasource_url" {
+  for_each = {
+    SPRING_DATASOURCE_URL           = var.database_url
+    SPRING_DATASOURCE_USERNAME      = var.dropbox_refresh_token
+    SPRING_DATASOURCE_PASSWORD      = var.dropbox_app_key
+  }
+}
+
+
 module "scraper" {
   source     = "./modules/scraper"
   name       = "classifieds-scraper"
   project_id = railway_project.classifieds-lifecycle.id
 }
 
-
-# module "scraper_test" {
-#   source     = "./modules/scraper"
-#   name       = "classifieds-scraper-test"
-#   project_id = railway_project.classifieds-lifecycle.id
-# }
 
 # The Backup Service
 resource "railway_service" "db_backup" {
@@ -33,7 +43,7 @@ resource "railway_service" "db_backup" {
   source_image  = "ghcr.io/tschy/classifieds-backup-service:latest"
 
   # This tells Railway to run it every night at midnight
-  cron_schedule = "*/5 * * * *"
+  cron_schedule = "0 0 * * *"
 }
 
 # Environment Variables for the service
@@ -50,4 +60,32 @@ resource "railway_variable" "db_backup_vars" {
 
   name       = each.key
   value      = each.value
+}
+
+# __generated__ by Terraform from "44f12661-648c-4127-9edb-6f62916c607a"
+resource "railway_project" "classifieds-lifecycle" {
+  default_environment = {
+    name = "production"
+  }
+  has_pr_deploys = false
+  name           = "classifieds-lifecycle"
+  private        = true
+  workspace_id   = "fdecf6ba-6b48-4c3c-b865-eecdc59311f2"
+}
+
+# __generated__ by Terraform from "91673c0b-c1a8-45ce-9eb2-5666d2aa63c0"
+resource "railway_service" "db" {
+  name          = "Postgres"
+  project_id    = "44f12661-648c-4127-9edb-6f62916c607a"
+  regions = [
+    {
+      num_replicas = 1
+      region       = "europe-west4-drams3a"
+    },
+  ]
+  source_image                   = "ghcr.io/railwayapp-templates/postgres-ssl:18"
+  volume = {
+    mount_path = "/var/lib/postgresql/data"
+    name       = "postgres-volume"
+  }
 }
